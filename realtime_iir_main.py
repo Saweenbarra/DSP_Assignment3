c2b8f11bcd542a0eb30bd1950ec73a49fa27be3d
Adam <adamfrew1999@gmail.com>
2020-11-20 20:23:18 +0000

    Computing coefficients

--- realtime_iir_main.py
+++ realtime_iir_main.py
@@ -1,10 +1,18 @@
 from pyfirmata2 import Arduino
-import time
+import scipy.signal as signal
+import numpy as np
+
+fs = 100
+fc = 3
+
+b,a = signal.butter(4,2*fc/fs)
+sos = signal.butter(4,2*fc/fs,output='sos')
+print(a)
 
 board = Arduino('/dev/ttyACM3')
 
-#Sample every 20ms
-board.samplingOn(20)
+#Sample every 1000/fs milliseconds
+board.samplingOn(1000/fs)
 
-board.analog[0].register_callback(myCallback)
-board.analog[0].enable_reporting()
+#board.analog[0].register_callback(myCallback)
+#board.analog[0].enable_reporting()
