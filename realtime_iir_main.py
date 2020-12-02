77bb95bd62a751a51e6fc6b4cf833a8a91fcb9c4
Adam <adamfrew1999@gmail.com>
2020-12-02 16:58:25 +0000

    Jitter included in main file

--- realtime_iir_main.py
+++ realtime_iir_main.py
@@ -4,10 +4,26 @@ import numpy as np
 import iir_filter as iir
 import matplotlib.pyplot as plt
 import RealtimePlotWindow as rtpw
+import time
+import threading
 
 unfilteredPlot = rtpw.RealtimePlotWindow()
 filteredPlot = rtpw.RealtimePlotWindow()
 
+sampleCount = 0
+timer_run = 1
+
+def timerfunc():
+    global sampleCount
+    global timer_run
+    while(timer_run):
+        time.sleep(10)
+        print(sampleCount)
+        sampleCount = 0
+
+
+
+timer = threading.Thread(target=timerfunc)
 
 fs = 100
 fc = 0.5
@@ -20,6 +36,9 @@ filter = iir.IIRFilter(sos)
 def myCallback(data):
     unfilteredPlot.addData(data)
     filteredPlot.addData(filter.filter(data))
+    global sampleCount
+    sampleCount += 1
+
 
 board = Arduino('/dev/ttyACM3')
 
@@ -28,8 +47,10 @@ board.samplingOn(1000/fs)
 
 board.analog[0].register_callback(myCallback)
 board.analog[0].enable_reporting()
+timer.start()
+
 
 plt.show()
-
+timer_run = 0
 # needs to be called to close the serial port
 board.exit()
