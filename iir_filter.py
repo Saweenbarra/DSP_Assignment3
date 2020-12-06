aaf556adbe0dfa69b3bcd7388c565f012d918ff5
Adam <adamfrew1999@gmail.com>
2020-12-06 17:26:08 +0000

    FUCK YOU I WONT DO WHAT YOU TELL ME!

--- iir_filter.py
+++ iir_filter.py
@@ -1,12 +1,8 @@
 #2nd order, form II, IIR filter class
 class IIR2Filter:
-    def __init__(self,_b0,_b1,_b2,_a0,_a1,_a2):
-        self.b0 = _b0
-        self.b1 = _b1
-        self.b2 = _b2
-        self.a0 = _a0
-        self.a1 = _a1
-        self.a2 = _a2
+    def __init__(self,_num,_den):
+        self.numerator = _num
+        self.denominator = _den
         self.buffer1 = 0
         self.buffer2 = 0
     
@@ -14,11 +10,11 @@ class IIR2Filter:
         inp=0.0
         out=0.0
         inp=v
-        out=(self.b1*self.buffer1)
-        inp=inp-(self.a1*self.buffer1)
-        out=out+(self.b2*self.buffer2)
-        inp=inp-(self.a2*self.buffer2)
-        out=out+inp*self.b0
+        out=(self.numerator[1]*self.buffer1)
+        inp=inp-(self.denominator[1]*self.buffer1)
+        out=out+(self.numerator[2]*self.buffer2)
+        inp=inp-(self.denominator[2]*self.buffer2)
+        out=out+inp*self.numerator[0]
         self.buffer2=self.buffer1
         self.buffer1=inp
         return out
@@ -28,7 +24,7 @@ class IIRFilter:
     def __init__(self, sos):
         self.IIR2filters = []
         for row in sos:
-            self.IIR2filters.append(IIR2Filter(row[0],row[1],row[2],row[3],row[4],row[5]))
+            self.IIR2filters.append(IIR2Filter(row[:3],row[3:]))
     
     def filter(self,v):
         intermediateOutput = v
