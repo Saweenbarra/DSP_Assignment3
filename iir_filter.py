788de7847f5e793f015ca895137f1ec7622e8ae9
Lewis Russell <lewisrussell@lewiss-mbp-2.home>
2020-12-06 20:26:55 +0000

    added some comments

--- iir_filter.py
+++ iir_filter.py
@@ -11,13 +11,14 @@ class IIR2Filter:
         out=0.0
         inp=v
 
+        #The IIR Feedback (Buffered feedback multiplied with IIR coefficients) is summed with the input
         inp=inp-(self.iirCoeff[1]*self.buffer1)
         inp=inp-(self.iirCoeff[2]*self.buffer2)
 
         out=(self.firCoeff[1]*self.buffer1)
         out=out+(self.firCoeff[2]*self.buffer2)
         out=out+inp*self.firCoeff[0]
-        
+
         self.buffer2=self.buffer1
         self.buffer1=inp
         return out
